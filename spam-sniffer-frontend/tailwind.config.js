4206de1f726716a51804aa66da524ee3f5a4bf83
Yashwanth Yalavali <yalavali.venkatasubbaiah22@st.niituniversity.in>
2025-04-22 20:54:31 +0530

    Phase-2 (Profile added)

--- spam-sniffer-frontend/tailwind.config.js
+++ spam-sniffer-frontend/tailwind.config.js
@@ -4,8 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
   },
+  
   plugins: [],
 }
 
